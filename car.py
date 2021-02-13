2c93a73f192ec8a64b9b5ad73ec0ec3c73a22642
hassan <hassan8ghannoum@gmail.com>
2021-02-13 02:16:12 +0200

    add accelerate to car class

--- car.py
+++ car.py
@@ -1,4 +1,29 @@
-class car:
+class Car:
+  """Car Object
+  """      
   def __init__(self):
       self.name = 'Mercedes'
       self.model = '1998'
+      self.velocity = 0
+
+
+  def accelerate(self, acceleration, time):
+    """increase the velocity of the car, by the value acceleration * time.
+    Args:
+        acceleration ([float]): the acceleration value
+        time ([time]): the time of the acceleration
+
+    """     
+    self.velocity += acceleration * time   
+
+# define a crv car.
+crv = Car()
+crv.accelerate(10, 10)
+
+print(crv.velocity)
+
+
+
+
+
+
